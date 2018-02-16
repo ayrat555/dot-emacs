b152e7694b39db8a5b9be61d63b05e73d9428b73
Ayrat Badykov <ayratin555@gmail.com>
2018-02-16 10:00:55 +0300

    use enhaced ruby mode

--- lisp/init-ruby-mode.el
+++ lisp/init-ruby-mode.el
@@ -1,8 +1,11 @@
 (require 'rubocop)
 (require 'rspec-mode)
 
-;; Do not insert encodinf in ruby files
-(setq ruby-insert-encoding-magic-comment nil)
-(add-hook 'ruby-mode-hook #'rubocop-mode)
+(autoload 'enh-ruby-mode "enh-ruby-mode" "Major mode for ruby files" t)
+(add-to-list 'auto-mode-alist '("\\.rb$" . enh-ruby-mode))
+(add-to-list 'interpreter-mode-alist '("ruby" . enh-ruby-mode))
+(add-hook 'enh-ruby-mode #'rubocop-mode)
+;; don't deep indent arrays and hashes
+(setq enh-ruby-deep-indent-paren nil)
 
 (provide 'init-ruby-mode)

c0926eaecc6053c629199a00dbeac3e2115787c1
Ayrat Badykov <ayratin555@gmail.com>
2021-08-01 18:20:56 +0300

    migrate to org-roam v2

--- lisp/init-org-mode.el
+++ lisp/init-org-mode.el
@@ -69,50 +69,17 @@
 
 (use-package org-roam
       :ensure t
-      :hook
-      (after-init . org-roam-mode)
       :custom
-      (org-roam-directory "~/.org-roam/")
-      (org-roam-dailies-directory "~/.org-roam/daily/")
-      (set org-roam-dailies-capture-templates
-           '(("t" "todo" entry
-              #'org-roam-capture--get-point
-              "* %?"
-              :file-name "daily/%<%Y-%m-%d>"
-              :head "#+title: %<%Y-%m-%d>\n"
-              :olp ("Todo"))
-
-             ("j" "journal" entry
-              #'org-roam-capture--get-point
-              "* %?"
-              :file-name "daily/%<%Y-%m-%d>"
-              :head "#+title: %<%Y-%m-%d>\n"
-              :olp ("Journal"))))
-      (org-roam-graph-viewer "open")
-      :bind (:map org-roam-mode-map
-              (("C-c n l" . org-roam)
-               ("C-c n f" . org-roam-find-file)
-               ("C-c n g" . org-roam-graph)
-               ("C-c n j" . org-roam-dailies-capture-today)
-               ("C-c n t" . org-roam-dailies-capture-tomorrow))
-              :map org-mode-map
-              (("C-c n i" . org-roam-insert))
-              (("C-c n I" . org-roam-insert-immediate))))
-
-(use-package org-roam-server
-  :ensure t
-  :config
-  (setq org-roam-server-host "127.0.0.1"
-        org-roam-server-port 8080
-        org-roam-server-authenticate nil
-        org-roam-server-export-inline-images t
-        org-roam-server-serve-files nil
-        org-roam-server-served-file-extensions '("pdf" "mp4" "ogv")
-        org-roam-server-network-poll t
-        org-roam-server-network-arrows nil
-        org-roam-server-network-label-truncate t
-        org-roam-server-network-label-truncate-length 60
-        org-roam-server-network-label-wrap-length 20))
+      (org-roam-directory (file-truename "~/.org-roam/"))
+      :bind (("C-c n l" . org-roam-buffer-toggle)
+             ("C-c n f" . org-roam-node-find)
+             ("C-c n g" . org-roam-graph)
+             ("C-c n i" . org-roam-node-insert)
+             ("C-c n c" . org-roam-capture)
+             ;; Dailies
+             ("C-c n j" . org-roam-dailies-capture-today))
+      :config
+      (org-roam-setup))
 
 (use-package olivetti
   :config

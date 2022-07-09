034e3db5fede4ddd8e889b392de95aca0d43f2d0
Ayrat Badykov <ayratin555@gmail.com>
2022-07-09 14:46:50 +0300

    pomidor improvements, update packages

--- lisp/init-pomidor.el
+++ lisp/init-pomidor.el
@@ -9,7 +9,11 @@
                              "afplay"
                              file))))
   (setq pomidor-sound-tick nil
-                pomidor-sound-tack nil)
+        pomidor-sound-tack nil)
+  (setq pomidor-seconds (* 15 60)) ; 15 minutes for the work period
+  (setq pomidor-break-seconds (* 5 60)) ; 5 minutes break time
+  (setq pomidor-breaks-before-long 4) ; wait 4 short breaks before long break
+  (setq pomidor-long-break-seconds (* 20 60)) ; 20 minutes long break time
   :hook (pomidor-mode . (lambda ()
                           (display-line-numbers-mode -1) ; Emacs 26.1+
                           (setq left-fringe-width 0 right-fringe-width 0)

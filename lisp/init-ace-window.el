1b9ece364f8d36c40bb3f0e6ed87e87a3545d9f4
Ayrat Badykov <ayratin555@gmail.com>
2022-08-22 09:06:56 +0300

    update packages / add additional global key for 'ace-window

--- lisp/init-ace-window.el
+++ lisp/init-ace-window.el
@@ -1,5 +1,6 @@
 (use-package ace-window
   :config
-  (global-set-key (kbd "M-o") 'ace-window))
+  (global-set-key (kbd "M-o") 'ace-window)
+  (global-set-key (kbd "M-p") 'ace-window))
 
 (provide 'init-ace-window)

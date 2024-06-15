ba6b222148c94d8add3d07260fb09d12fe6e8907
Ayrat Badykov <ayratin555@gmail.com>
2024-06-15 10:29:01 +0300

    update packages and change theme

--- lisp/init-themes.el
+++ lisp/init-themes.el
@@ -19,8 +19,8 @@
 (if (daemonp)
     (add-hook 'after-make-frame-functions
               (lambda (frame)
-                (load-theme 'doom-xcode t)))
-    (load-theme 'doom-xcode t))
+                (load-theme 'doom-bluloco-dark t)))
+    (load-theme 'doom-bluloco-dark t))
 
 (use-package smart-mode-line-atom-one-dark-theme
   :ensure t)
